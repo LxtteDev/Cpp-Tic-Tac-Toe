a153c3127ee0556ec2979c5dcb7d1538d5d53a64
LxtteDev <scott.engelbrecht2007@gmail.com>
2023-05-24 16:42:44 -0700

    Render window

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main(int, char**) {
-    std::cout << "Hello, world!\n";
+
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Tic Tac Toe");
+
+     while (window.isOpen()) {
+        // sf::Time deltaTime = clock.restart();
+        // float deltaSeconds = deltaTime.asSeconds();
+
+        sf::Event e;
+		while (window.pollEvent(e))
+			if (e.type == sf::Event::Closed)
+                window.close();
+
+        std::cout << "Drawing scene" << std::endl;
+        window.display();
+    }
+
+    window.close();
 }
