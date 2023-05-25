80ca203dc1c0a08e54302d06b1f45f9165f8406c
LxtteDev <scott.engelbrecht2007@gmail.com>
2023-05-24 17:41:32 -0700

    Board

--- src/main.cpp
+++ src/main.cpp
@@ -4,30 +4,45 @@
 const int unsigned barSize = 4;
 
 void drawLines(sf::RenderWindow& window, sf::Vector2u size) {
-    if (size.x >= size.y) {
-        int yHalf = size.y / 2;
-        int xHalf = size.x / 2;
+    int yHalf = size.y / 2;
+    int xHalf = size.x / 2;
 
-        // Not very complex math
-        int height = size.y - 2 * barSize;
+    // Not very complex math
+    int bar = (size.x >= size.y ? size.y : size.x) - 2 * barSize;
 
-        int y = yHalf - height / 2;
-        int x1 = xHalf - height / 6 - 2; 
-        int x2 = xHalf + height / 6 - 2; 
+    int y = yHalf - bar / 2;
+    int x1 = xHalf - bar / 6 - 2; 
+    int x2 = xHalf + bar / 6 - 2; 
 
-        // Vertical Bars
-        sf::RectangleShape vert1;
-        sf::RectangleShape vert2;
+    int x = xHalf - bar / 2;
+    int y1 = yHalf - bar / 6 - 2; 
+    int y2 = yHalf + bar / 6 - 2; 
 
-        vert1.setSize(sf::Vector2f(barSize, height));
-        vert2.setSize(sf::Vector2f(barSize, height));
-    
-        vert1.setPosition(sf::Vector2f(x1, y));
-        vert2.setPosition(sf::Vector2f(x2, y));
-    
-        window.draw(vert1);
-        window.draw(vert2);
-    }
+    // Vertical Bars
+    sf::RectangleShape vert1;
+    sf::RectangleShape vert2;
+
+    vert1.setSize(sf::Vector2f(barSize, bar));
+    vert2.setSize(sf::Vector2f(barSize, bar));
+
+    vert1.setPosition(sf::Vector2f(x1, y));
+    vert2.setPosition(sf::Vector2f(x2, y));
+
+    window.draw(vert1);
+    window.draw(vert2);
+
+    // Horizontal bars
+    sf::RectangleShape horz1;
+    sf::RectangleShape horz2;
+
+    horz1.setSize(sf::Vector2f(bar, barSize));
+    horz2.setSize(sf::Vector2f(bar, barSize));
+
+    horz1.setPosition(sf::Vector2f(x, y1));
+    horz2.setPosition(sf::Vector2f(x, y2));
+
+    window.draw(horz1);
+    window.draw(horz2);
 }
 
 int main(int, char**) {
