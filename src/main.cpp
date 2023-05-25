185116a4f4e2a7465bee8346d18cbab4aa627d74
LxtteDev <scott.engelbrecht2007@gmail.com>
2023-05-24 21:00:00 -0700

    Revert "Added board"
    This reverts commit 3a68f660fdfea170cefba01190a16bc9f17e391a.

--- src/main.cpp
+++ src/main.cpp
@@ -9,9 +9,6 @@ int board[3][3] = { // -1 -> Empty; 0 -> X; 1 -> O
 };
 bool turn = 0; // 0 -> X; 1 -> O
 
-sf::Texture xImage;
-sf::Texture oImage;
-
 void drawLines(sf::RenderWindow& window, sf::Vector2u size) {
     int yHalf = size.y / 2;
     int xHalf = size.x / 2;
@@ -65,39 +62,14 @@ void click(sf::Vector2u size, sf::Vector2i position) {
         int x = mousePosition.x > boardSizeThree * 2 ? 2 : mousePosition.x > boardSizeThree ? 1 : 0;
         int y = mousePosition.y > boardSizeThree * 2 ? 2 : mousePosition.y > boardSizeThree ? 1 : 0;
         
-        if (board[x][y] == -1) {
-            board[x][y] = turn;
-            turn = !turn;
-        }
-    }
-}
-
-void displayBoard(sf::RenderWindow& window, sf::Vector2u size) {
-    int boardSize = std::min(size.x, size.y) - barSize * 2;
-    int tileSize = (boardSize - boardSize % 3) / 3 - barSize * 2;
-    sf::Vector2i boardPosition = size.x >= size.y ? sf::Vector2i((size.x - boardSize) / 2, barSize) : sf::Vector2i(barSize, (size.y - boardSize) / 2);
-
-    for (unsigned int y = 0; y < 3; y++) {
-        for (unsigned int x = 0; x < 3; x++) {
-            int tileState = board[x][y];
-
-            if (tileState == -1) continue;
-
-            sf::Sprite tile;
-            tile.setPosition(x * tileSize + (x + 1) * barSize + boardPosition.x, y * tileSize + (y + 1) * barSize + boardPosition.y);
-            tile.setScale((float)tileSize / 64.0f, (float)tileSize / 64.0f);
-            tile.setTexture(tileState == 0 ? xImage : oImage);
-
-            window.draw(tile);
-        }
+        if (board[x][y] == -1) return;
+        board[x][y] = turn;
+        turn = !turn;
     }
 }
 
 int main(int, char**) {
     sf::RenderWindow window(sf::VideoMode(800, 600), "Tic Tac Toe");
-
-    xImage.loadFromFile("img/X.png");
-    oImage.loadFromFile("img/O.png");
 
     bool mouseLock = false;
 
@@ -119,8 +91,8 @@ int main(int, char**) {
 
         window.clear();
 
+        // Render lines
         drawLines(window, window.getSize());
-        displayBoard(window, window.getSize());
 
         window.display();
     }
